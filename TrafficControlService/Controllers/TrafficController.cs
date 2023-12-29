e5e482fb66d272a9a9e2dd95667c756a77ea8a8d
fatsug <1nd13r0ck@gmail.com>
2023-12-29 17:29:58 -0600

    Step 3

--- TrafficControlService/Controllers/TrafficController.cs
+++ TrafficControlService/Controllers/TrafficController.cs
@@ -2,26 +2,14 @@
 
 [ApiController]
 [Route("")]
-public class TrafficController : ControllerBase
+public class TrafficController(
+    ILogger<TrafficController> logger,
+    HttpClient httpClient,
+    IVehicleStateRepository vehicleStateRepository,
+    ISpeedingViolationCalculator speedingViolationCalculator)
+    : ControllerBase
 {
-    private readonly HttpClient _httpClient;
-    private readonly IVehicleStateRepository _vehicleStateRepository;
-    private readonly ILogger<TrafficController> _logger;
-    private readonly ISpeedingViolationCalculator _speedingViolationCalculator;
-    private readonly string _roadId;
-
-    public TrafficController(
-        ILogger<TrafficController> logger,
-        HttpClient httpClient,
-        IVehicleStateRepository vehicleStateRepository,
-        ISpeedingViolationCalculator speedingViolationCalculator)
-    {
-        _logger = logger;
-        _httpClient = httpClient;
-        _vehicleStateRepository = vehicleStateRepository;
-        _speedingViolationCalculator = speedingViolationCalculator;
-        _roadId = speedingViolationCalculator.GetRoadId();
-    }
+    private readonly string _roadId = speedingViolationCalculator.GetRoadId();
 
     [HttpPost("entrycam")]
     public async Task<ActionResult> VehicleEntry(VehicleRegistered msg)
@@ -29,8 +17,11 @@ public class TrafficController : ControllerBase
         try
         {
             // log entry
-            _logger.LogInformation($"ENTRY detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")} " +
-                $"of vehicle with license-number {msg.LicenseNumber}.");
+            logger.LogInformation(
+                "ENTRY detected in lane {Lane} at {Time} of vehicle with license-number {LicenseNumber}",
+                msg.Lane,
+                msg.Timestamp.ToString("hh:mm:ss"),
+                msg.LicenseNumber);
 
             // store vehicle state
             var vehicleState = new VehicleState
@@ -38,7 +29,7 @@ public class TrafficController : ControllerBase
                 LicenseNumber = msg.LicenseNumber,
                 EntryTimestamp = msg.Timestamp
             };
-            await _vehicleStateRepository.SaveVehicleStateAsync(vehicleState);
+            await vehicleStateRepository.SaveVehicleStateAsync(vehicleState);
 
             return Ok();
         }
@@ -54,40 +45,45 @@ public class TrafficController : ControllerBase
         try
         {
             // get vehicle state
-            var vehicleState = await _vehicleStateRepository.GetVehicleStateAsync(msg.LicenseNumber);
+            var vehicleState = await vehicleStateRepository.GetVehicleStateAsync(msg.LicenseNumber);
             if (!vehicleState.HasValue)
             {
                 return NotFound();
             }
 
             // log exit
-            _logger.LogInformation($"EXIT detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")} " +
-                $"of vehicle with license-number {msg.LicenseNumber}.");
+            logger.LogInformation(
+                "EXIT detected in lane {Lane} at {Time} of vehicle with license-number {LicenseNumber}",
+                msg.Lane,
+                msg.Timestamp.ToString("hh:mm:ss"),
+                msg.LicenseNumber);
 
             // update state
-            vehicleState = vehicleState.Value with { ExitTimestamp = msg.Timestamp };
-            await _vehicleStateRepository.SaveVehicleStateAsync(vehicleState.Value);
+            vehicleState = vehicleState.Value with {ExitTimestamp = msg.Timestamp};
+            await vehicleStateRepository.SaveVehicleStateAsync(vehicleState.Value);
 
             // handle possible speeding violation
-            int violation = _speedingViolationCalculator.DetermineSpeedingViolationInKmh(
+            var violation = speedingViolationCalculator.DetermineSpeedingViolationInKmh(
                 vehicleState.Value.EntryTimestamp, vehicleState.Value.ExitTimestamp.Value);
-            if (violation > 0)
+            
+            if (violation <= 0) return Ok();
+            
+            logger.LogInformation(
+                "Speeding violation detected ({Violation} KMh) of vehicle with license-number {LicenseNumber}", 
+                violation, 
+                vehicleState.Value.LicenseNumber);
+
+            var speedingViolation = new SpeedingViolation
             {
-                _logger.LogInformation($"Speeding violation detected ({violation} KMh) of vehicle" +
-                    $"with license-number {vehicleState.Value.LicenseNumber}.");
+                VehicleId = msg.LicenseNumber,
+                RoadId = _roadId,
+                ViolationInKmh = violation,
+                Timestamp = msg.Timestamp
+            };
 
-                var speedingViolation = new SpeedingViolation
-                {
-                    VehicleId = msg.LicenseNumber,
-                    RoadId = _roadId,
-                    ViolationInKmh = violation,
-                    Timestamp = msg.Timestamp
-                };
-
-                // publish speedingviolation
-                var message = JsonContent.Create<SpeedingViolation>(speedingViolation);
-                await _httpClient.PostAsync("http://localhost:6001/collectfine", message);
-            }
+            // publish speedingviolation
+            var message = JsonContent.Create<SpeedingViolation>(speedingViolation);
+            await httpClient.PostAsync("http://localhost:3600/v1.0/publish/pubsub/speedingviolations", message);
 
             return Ok();
         }
