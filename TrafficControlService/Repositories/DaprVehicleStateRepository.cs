6511e03e573715c91bff6e13848d00c751f24b54
fatsug <1nd13r0ck@gmail.com>
2024-01-04 19:11:30 -0600

    Step 3: Use Dapr state management with the Dapr SDK for .NET

--- TrafficControlService/Repositories/DaprVehicleStateRepository.cs
+++ TrafficControlService/Repositories/DaprVehicleStateRepository.cs
@@ -1,30 +1,16 @@
 namespace TrafficControlService.Repositories;
 
-public class DaprVehicleStateRepository(HttpClient httpClient) : IVehicleStateRepository
+public class DaprVehicleStateRepository(DaprClient daprClient) : IVehicleStateRepository
 {
     private const string DAPR_STORE_NAME = "statestore";
 
     public async Task SaveVehicleStateAsync(VehicleState vehicleState)
     {
-        var state = new[]
-        {
-            new 
-            { 
-                key = vehicleState.LicenseNumber,
-                value = vehicleState
-            }
-        };
-
-        await httpClient.PostAsJsonAsync(
-            $"http://localhost:3600/v1.0/state/{DAPR_STORE_NAME}",
-            state);
+        await daprClient.SaveStateAsync(DAPR_STORE_NAME, vehicleState.LicenseNumber, vehicleState);
     }
 
     public async Task<VehicleState?> GetVehicleStateAsync(string licenseNumber)
     {
-        var state = await httpClient.GetFromJsonAsync<VehicleState>(
-            $"http://localhost:3600/v1.0/state/{DAPR_STORE_NAME}/{licenseNumber}");
-        
-        return state;
+        return await daprClient.GetStateAsync<VehicleState>(DAPR_STORE_NAME, licenseNumber);
     }
 }
