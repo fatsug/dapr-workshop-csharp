b617b1cda270d0ef681076b937cb499529dda79e
fatsug <1nd13r0ck@gmail.com>
2024-01-05 08:29:06 -0600

    lipstick/formatting

--- Simulation/Program.cs
+++ Simulation/Program.cs
@@ -1,9 +1,10 @@
-﻿int lanes = 3;
-CameraSimulation[] cameras = new CameraSimulation[lanes];
+﻿const int lanes = 3;
+var cameras = new CameraSimulation[lanes];
 var httpClient = new HttpClient();
+
 for (var i = 0; i < lanes; i++)
 {
-    int camNumber = i + 1;
+    var camNumber = i + 1;
     var trafficControlService = new HttpTrafficControlService(httpClient);
     cameras[i] = new CameraSimulation(camNumber, trafficControlService);
 }
